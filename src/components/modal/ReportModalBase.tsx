e81c30a482e2135fd774c4b215f575a8823c9978
jjub0217 <noonssub1004@gmail.com>
2026-03-13 16:44:09 +0900

    fix: reasonCode defaultValue 빈 문자열로 복원(#495)

--- src/components/modal/ReportModalBase.tsx
+++ src/components/modal/ReportModalBase.tsx
@@ -43,6 +43,7 @@ export default function ReportModalBase({ isOpen, heading, description, reasons,
   } = useForm<ReportFormValues>({
     mode: 'onChange',
     defaultValues: {
+      reasonCode: '',
       detailReason: '',
       imageFiles: [],
     },
